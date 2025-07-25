e78b31a039d9c90ac1e71881174a5c720823f1ed
estherngwasi <102970841+estherngwasi@users.noreply.github.com>
2025-07-25 17:38:50 +0300

    Update cartSlice.js

--- client/src/features/cart/cartSlice.js
+++ client/src/features/cart/cartSlice.js
@@ -1,3 +1,4 @@
+const API_URL = import.meta.env.VITE_API_URL || '';
 import { createSlice } from '@reduxjs/toolkit';
 
 const cartFromStorage = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
